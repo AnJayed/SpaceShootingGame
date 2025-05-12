349d7aa0454e1b06314ec7dfc1312dbd3c686ac7
Bazanji0 <142678534+Bazanji0@users.noreply.github.com>
2025-05-12 09:57:06 +0200

    primo commit

---
+++ script.js
@@ -0,0 +1,156 @@
+"use strict";
+
+// Ottieni il riferimento al canvas e al suo contesto 2D
+let canvas = document.querySelector("canvas");
+let c = canvas.getContext("2d");
+
+let suonoSparoPlayer = new Audio("sparo.wav");
+
+// Imposta le dimensioni del canvas pari a quelle della finestra
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Navicella {
+  constructor() {
+    this.velocita = {
+      x: 0,
+      y: 0,
+    };
+
+    this.immagine = new Image();
+    this.immagine.src = "navicella.png"; // Assicurati che l'immagine esista
+
+    this.dimensioneNavicella = 0.55;
+
+    this.width = 100;
+    this.height = 100;
+
+    this.posizione = {
+      x: canvas.width / 2 - this.width / 2,
+      y: canvas.height - this.height - 50,
+    };
+
+    this.immagine.onload = () => {
+      this.width = this.immagine.width * this.dimensioneNavicella;
+      this.height = this.immagine.height * this.dimensioneNavicella;
+      this.posizione.x = canvas.width / 2 - this.width / 2;
+      this.posizione.y = canvas.height - this.height - 50;
+    };
+  }
+
+  genera() {
+    c.drawImage(
+      this.immagine,
+      this.posizione.x,
+      this.posizione.y,
+      this.width,
+      this.height
+    );
+  }
+
+  aggiorna() {
+    this.posizione.x += this.velocita.x;
+    this.posizione.y += this.velocita.y;
+
+    if (this.posizione.x < 0) this.posizione.x = 0;
+    if (this.posizione.x + this.width > canvas.width)
+      this.posizione.x = canvas.width - this.width;
+    if (this.posizione.y < 0) this.posizione.y = 0;
+    if (this.posizione.y + this.height > canvas.height)
+      this.posizione.y = canvas.height - this.height;
+  }
+}
+
+class Proiettile {
+  constructor({ posizione, velocita }) {
+    this.posizione = posizione;
+    this.velocita = velocita;
+    this.radius = 5;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.posizione.x, this.posizione.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = "red";
+    c.fill();
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.posizione.x += this.velocita.x;
+    this.posizione.y += this.velocita.y;
+  }
+}
+
+// Crea il giocatore e la lista dei proiettili
+let player = new Navicella();
+let listaProiettili = [];
+
+// Funzione di animazione continua
+function animazione() {
+  requestAnimationFrame(animazione);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  player.aggiorna();
+  player.genera();
+
+  listaProiettili.forEach((p) => {
+    p.update();
+    
+    
+  });
+}
+
+// Tasti premuti
+let tasti = {
+  tastoSinistro: false,
+  tastoDestro: false,
+};
+
+addEventListener("keydown", ({ key }) => {
+  switch (key) {
+    case "ArrowLeft":
+      tasti.tastoSinistro = true;
+      player.velocita.x = -5;
+      break;
+    case "ArrowRight":
+      tasti.tastoDestro = true;
+      player.velocita.x = 5;
+      break;
+
+    case " ": 
+      listaProiettili.push(
+        new Proiettile({
+          posizione: {
+            x: player.posizione.x + player.width / 2,
+            y: player.posizione.y,
+          },
+          velocita: {
+            x: 0,
+            y: -7,
+          },
+        })
+      );
+      suonoSparoPlayer.currentTime = 0; 
+      suonoSparoPlayer.play();
+      break;
+  }
+});
+
+addEventListener("keyup", ({ key }) => {
+  switch (key) {
+    case "ArrowLeft":
+      tasti.tastoSinistro = false;
+      if (!tasti.tastoDestro) player.velocita.x = 0;
+      break;
+    case "ArrowRight":
+      tasti.tastoDestro = false;
+      if (!tasti.tastoSinistro) player.velocita.x = 0;
+      break;
+
+  }
+});
+
+// Avvia l'animazione
+animazione();
