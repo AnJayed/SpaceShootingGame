a2e96273f156dd54aa7b22763754677bb642db4d
AnJayed <anouar.jayed@iisgubbio.edu.it>
2025-05-21 10:48:15 +0200

    aggiunta punteggio

--- script.js
+++ script.js
@@ -4,22 +4,32 @@
 let canvas = document.querySelector("canvas");
 let c = canvas.getContext("2d");
 
+// Suono dello sparo
 let suonoSparoPlayer = new Audio("sparo.wav");
 
-
-
-// Imposta le dimensioni del canvas pari a quelle della finestra
+// Imposta le dimensioni del canvas
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let testoPunteggio = document.createElement("div");
+testoPunteggio.textContent = "Punteggio : 0";
+document.body.appendChild(testoPunteggio);
+testoPunteggio.style.color = "white";
+testoPunteggio.style.position = "absolute"; // Posiziona il div sopra il canvas
+testoPunteggio.style.top = "20px"; // Distanza dal top
+testoPunteggio.style.left = "20px"; // Distanza dalla sinistra
+testoPunteggio.style.fontSize = "24px"; // Font più grande per visibilità
+
+let punteggio = 0;
+
+function aggiornaPunteggio() {
+  testoPunteggio.textContent = "Punteggio : " + punteggio;
+}
 
 function creaNavicella() {
   let navicella = {
-    vita : 100,
-    velocita: {
-      x: 0,
-      y: 0,
-    },
+    vita: 100,
+    velocita: { x: 0, y: 0 },
     immagine: new Image(),
     dimensioneNavicella: 0.55,
     width: 100,
@@ -28,7 +38,7 @@ function creaNavicella() {
       x: canvas.width / 2 - 50,
       y: canvas.height - 150,
     },
-    
+
     genera() {
       c.drawImage(
         this.immagine,
@@ -38,9 +48,9 @@ function creaNavicella() {
         this.height
       );
     },
+
     aggiorna() {
       this.posizione.x += this.velocita.x;
-      this.posizione.y += this.velocita.y;
 
       if (this.posizione.x < 0) this.posizione.x = 0;
       if (this.posizione.x + this.width > canvas.width)
@@ -62,11 +72,9 @@ function creaNavicella() {
   return navicella;
 }
 
-
-
 function creaProiettile({ posizione, velocita }) {
   return {
-    posizione,
+    posizione: {...posizione },
     velocita,
     radius: 5,
     draw() {
@@ -77,35 +85,84 @@ function creaProiettile({ posizione, velocita }) {
       c.closePath();
     },
     update() {
-      this.draw();
       this.posizione.x += this.velocita.x;
       this.posizione.y += this.velocita.y;
+      this.draw();
     },
   };
 }
 
+function creaMostro(x, y) {
+  let immagine = new Image();
+  immagine.src = "spaceinvader.png";
 
+  return {
+    posizione: { x, y },
+    width: 100,
+    height: 100,
+    velocita: 2,
+    immagine: immagine,
+    draw() {
+      c.drawImage(this.immagine, this.posizione.x, this.posizione.y, this.width, this.height);
+    },
+    update() {
+      this.posizione.y += this.velocita;
+      this.draw();
+    }
+  };
+}
+
+function rettangoliCollidono(proiettile, mostro) {
+  return (
+    proiettile.posizione.x > mostro.posizione.x &&
+    proiettile.posizione.x < mostro.posizione.x + mostro.width &&
+    proiettile.posizione.y > mostro.posizione.y &&
+    proiettile.posizione.y < mostro.posizione.y + mostro.height
+  );
+}
 
 let player = creaNavicella();
 let listaProiettili = [];
-
-function animazione() {
-  requestAnimationFrame(animazione);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-
-  player.aggiorna();
-  player.genera();
-
-  listaProiettili.forEach((proiettile) => {
-    proiettile.update();
-  });
-}
-
+let mostri = [];
 
 let tasti = {
   tastoSinistro: false,
   tastoDestro: false,
 };
+
+function animazione() {
+  requestAnimationFrame(animazione);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Player
+  player.aggiorna();
+  player.genera();
+
+  // Proiettili
+  listaProiettili.forEach((proiettile, i) => {
+    proiettile.update();
+
+    // Collisione con mostri
+    mostri.forEach((mostro, j) => {
+      if (rettangoliCollidono(proiettile, mostro)) {
+        listaProiettili.splice(i, 1);
+        mostri.splice(j, 1);
+        punteggio += 10;
+        aggiornaPunteggio();
+      
+      }
+    });
+  });
+  // Mostri
+  mostri.forEach((mostro) => {
+    mostro.update();
+  });
+}
+function aumentaVelocita(){
+  if(punteggio==50){
+    //TO DO
+  };
+}
 
 addEventListener("keydown", ({ key }) => {
   switch (key) {
@@ -124,10 +181,7 @@ addEventListener("keydown", ({ key }) => {
             x: player.posizione.x + player.width / 2,
             y: player.posizione.y,
           },
-          velocita: {
-            x: 0,
-            y: -7,
-          },
+          velocita: { x: 0, y: -7 },
         })
       );
       suonoSparoPlayer.currentTime = 0;
@@ -149,38 +203,9 @@ addEventListener("keyup", ({ key }) => {
   }
 });
 
-
-function creaMostro() {
-  let mostro = document.createElement("img");
-  mostro.src = "spaceinvader.png";
-  mostro.className = "mostro";
-  mostro.style.position = "absolute";
-  mostro.style.width = "100px";
-  mostro.style.height = "100px";
-  mostro.style.left = Math.random() * (window.innerWidth - 100) + "px";
-  mostro.style.top = "-100px";
-  
-
-  document.body.appendChild(mostro);
-}
-
-function aggiornaMostri() {
-  let mostri = document.querySelectorAll(".mostro");
-  mostri.forEach((mostro) => {
-    let y = parseInt(mostro.style.top);
-    y += 9;
-    mostro.style.top = y + "px";
-  });
-}
-
 setInterval(() => {
-  creaMostro();
+  let x = Math.random() * (canvas.width - 100);
+  mostri.push(creaMostro(x, -100));
 }, 2000);
-
-setInterval(() => {
-  aggiornaMostri();
-}, 300);
-
-
 
 animazione();
